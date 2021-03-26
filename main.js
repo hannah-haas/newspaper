ee293e79f2ad7e203b4ff3e9d14848625d3d546b
Hannah Haas <hannah_haas@brown.edu>
2021-03-26 00:09:06 -0400

    testing stuff

--- main.js
+++ main.js
@@ -1,15 +1,26 @@
-const logBtn = document.getElementById('log');
+const logBtn = document.getElementById('explore');
 logBtn.addEventListener('click', fetchData);
 
 async function fetchData() {
 
     const response = await fetch('https://hannah-haas.github.io/newspaper/article_list.json');
     const data = await response.json();
+    var random_article_list = []
 
     data.forEach(obj => {
-        Object.entries(obj).forEach(([key, value]) => {
-            console.log(`${key} ${value}`);
+        Object.entries(obj).forEach(([key, values]) => {
+
+            console.log(values);
+            if (values.length != 0){
+              random_article_list = random_article_list.concat(values)
+            }
+
         });
-        console.log('-------------------');
     });
+
+    random_num = Math.floor(Math.random() * Math.floor(random_article_list.length));
+    random_article = random_article_list[random_num]
+    console.log(random_article)
+    // document.location.href = random_article
+
 }
