4ff2f365f8413dbd666941af26c89870d8e1c925
Hannah Haas <hannah_haas@brown.edu>
2021-07-15 23:19:36 +0200

    connect button

--- explore_function.js
+++ explore_function.js
@@ -18,6 +18,8 @@ async function fetchData() {
   });
   random_num = Math.floor(Math.random() * Math.floor(random_article_list.length));
   random_article = random_article_list[random_num]
-  document.location.href = random_article
+  var url = "https://hannah-haas.github.io/newspaper/" + random_article
+  console.log(url)
+  document.location.href = url
 
 }
